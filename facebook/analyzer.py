134713ca755df6d8f50a9d836e737060e377bd1a
Wonhyuk Choi <woc997@g.harvard.edu>
2020-05-03 00:31:02 +0900

    create nlp

--- facebook/analyzer.py
+++ facebook/analyzer.py
@@ -1,38 +1,32 @@
 from utils import json_files_to_list
 from datetime import datetime
 from matplotlib import pyplot as plt
+from nlp import TextPreprocessor
 from utils import MarkdownGenerator
 
 
 # TODO: add graphing
-# TODO: add nlp
 # TODO: add auto-generate tables in markdown??
 class FbMessageAnalyzer:
     def __init__(self):
-        self._messages = []
-        self._sender_messages = {}
         self._senders = set()
+        self._messages = []
+        self._messages_text = []
+        self._sender_messages = {}
+        self._freq_words = {}
+        self._sender_freq_words = {}
         self._time = []
 
     def load_from_dir(self, path: str, sender='sender_name',
                       content='content', time='timestamp_ms') -> None:
         self._messages = json_files_to_list(path=path)
         self._parse_messages(sender=sender, content=content)
-        self._parse_time(time=time)
+        self._parse_time(time_type=time)
+        self._get_freq_words()
 
     def count_by_person(self) -> dict:
         return {sender: len(messages) for sender, messages
                 in self._sender_messages}
-
-    def time_series(self, timestamp_div=1000) -> list:
-        datetime_list = []
-        for timestamp in self._time:
-            timestamp = timestamp / timestamp_div
-            datetime_list += datetime.fromtimestamp(timestamp)
-        return datetime_list
-
-    def generate_graphs(self):
-        raise NotImplementedError
 
     def write_markdown(self, file_name: str):
         md_maker = MarkdownGenerator()
@@ -41,15 +35,42 @@ class FbMessageAnalyzer:
 
     def _parse_messages(self, sender='sender_name',
                         content='content') -> None:
+
         for msg in self._messages:
             if content in msg.keys():
-                self._sender_messages.\
-                    setdefault(msg[sender], []).append(msg[content])
+                text = msg[content]
             else:
-                self._sender_messages.\
-                    setdefault(msg[sender], []).append('<other>')
+                text = '<other>'
+
+            self._sender_messages.\
+                setdefault(msg[sender], []).append(text)
+            self._messages_text.append(text)
+
         self._senders = set(self._sender_messages.keys())
 
-    def _parse_time(self, time='timestamp_ms'):
+    def _parse_time(self, time_type='timestamp_ms'):
+        if time_type == 'timestamp_ms':
+            timestamp_div = 1000
+        else:
+            raise NotImplementedError
+
         for msg in self._messages:
-            self._time += msg[time]
+            timestamp = msg[time_type] / timestamp_div
+            self._time.append(datetime.fromtimestamp(timestamp))
+
+    def _get_freq_words(self):
+        text_preprocessor = TextPreprocessor()
+        self._freq_words = text_preprocessor.most_freq(
+            " ".join(self._messages_text))
+        for sender, messages in self._sender_messages.items():
+            sender_freq = text_preprocessor.most_freq(" ".join(messages))
+            self._sender_freq_words[sender] = sender_freq
+
+    @staticmethod
+    def _piechart(data: dict, save_name: str):
+        plt.pie(data)
+        plt.savefig(save_name)
+
+    @staticmethod
+    def _linechart(data: list, save_name: str):
+        raise NotImplementedError
--- facebook/nlp.py
+++ facebook/nlp.py
@@ -1,10 +1,53 @@
-import nltk
+from string import punctuation
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+from wrappers import timer
 
 
-class Preprocessor:
-    def remove_punkt(self, text=''):
-        raise NotImplementedError
+class TextPreprocessor:
+    def __init__(self, lang='eng'):
+        self._lang = lang.strip().lower()
+        if self._lang != 'eng':
+            raise NotImplementedError
 
-    def lemmatize(self, text=''):
-        raise NotImplementedError
+    @timer
+    def preprocess(self, text: str, ascii_only=True, rm_stopword=True):
+        if ascii_only:
+            text = "".join(char for char in text if ord(char) < 128)
+
+        text = "".join(char for char in text if char not in punctuation)
+
+        if rm_stopword:
+            if self._lang != 'eng':
+                raise NotImplementedError
+
+            word_list = text.split()
+            text = " ".join(word for word in word_list
+                            if word not in stopwords.words('english'))
+
+        return text
+
+    @staticmethod
+    @timer
+    def lemmatize(text: str, min_word_len=2):
+        lemmatizer = WordNetLemmatizer()
+        word_list = text.split()
+        lemmatized = tuple(lemmatizer.lemmatize(word) for word in word_list)
+        pruned_text = " ".join(word for word in lemmatized
+                               if len(word) >= min_word_len)
+        return pruned_text
+
+    @timer
+    def most_freq(self, text: str, preprocess=True, lemmatize=True, max_num=None,
+                  ascii_only=True, rm_stopword=True, min_word_len=2):
+
+        if preprocess:
+            text = self.preprocess(text, ascii_only=ascii_only,
+                                   rm_stopword=rm_stopword)
+        if lemmatize:
+            text = self.lemmatize(text, min_word_len=min_word_len)
+
+        word_list = text.split()
+        freq_words = Counter(word_list).most_common(max_num)
+        return freq_words
